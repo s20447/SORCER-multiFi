46b636035aee5f64e48e02751b813624920c30ed
Paszczak <pawel.paszkowski@student.wat.edu.pl>
2024-01-24 01:10:07 +0100

    minor fixes to MonitorImpl

--- examples/sml/src/main/java/sorcer/arithmetic/provider/impl/MonitorImpl.java
+++ examples/sml/src/main/java/sorcer/arithmetic/provider/impl/MonitorImpl.java
@@ -9,6 +9,7 @@ public class MonitorImpl implements IMonitor{
     private final int DAY_COUNT = 7;
     private final int CALORIE_TO_STEP_COEFF = 0.03;
 
+    private int currentDay;
     private float [] dailyHeartRate;
     private float [] dailyBloodPressure;
     private float [] dailyBloodSugar;
@@ -16,11 +17,12 @@ public class MonitorImpl implements IMonitor{
     private float [] nightlySleepDuration;
 
     public MonitorImpl() {
-        dailyHeartRate = new float[DAY_COUNT];
-        dailyBloodPressure = new float[DAY_COUNT];
-        dailyBloodSugar = new float[DAY_COUNT];
-        dailySteps = new int[DAY_COUNT];
-        nightlySleepDuration = new float[DAY_COUNT];
+        currentDay = 0;
+        this.dailyHeartRate = new float[DAY_COUNT];
+        this.dailyBloodPressure = new float[DAY_COUNT];
+        this.dailyBloodSugar = new float[DAY_COUNT];
+        this.dailySteps = new int[DAY_COUNT];
+        this.nightlySleepDuration = new float[DAY_COUNT];
     }
 
     private float getAverage(float[] arr) {
@@ -31,6 +33,15 @@ public class MonitorImpl implements IMonitor{
 
     private float stepsToCalories(int steps) {
         return steps * CALORIE_TO_STEP_COEFF;
+    }
+
+    public void dailyUpdate(float heartRate, float bloodPressure, float bloodSugar, int steps, float sleepDuration) {
+        this.dailyHeartRate[currentDay] = heartRate;
+        this.dailyBloodPressure[currentDay] = bloodPressure;
+        this.dailyBloodSugar[currentDay] = bloodSugar;
+        this.dailySteps[currentDay] = steps;
+        this.nightlySleepDuration[currentDay] = sleepDuration;
+        this.currentDay = (currentDay + 1) % DAY_COUNT;
     }
 
     public MonitorData getMonitorData() {
